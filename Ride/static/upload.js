13d543ee0f82c3738f075e80003525c2650308bf
TaekeonLee <taekeon.m.lee@gmail.com>
2025-10-13 18:46:45 -0700

    add html for exterior and interior, set default for year in js file

--- Ride/static/upload.js
+++ Ride/static/upload.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const select = document.getElementById("year");
+    const yearSelect = document.getElementById("year");
+    const modelSelect = document.getElementById("model");
+    const trimSelect = document.getElementById("trim");
+
     const startYear = 2023;
     const endYear = new Date().getFullYear() + 1;
 
@@ -8,7 +11,40 @@ document.addEventListener("DOMContentLoaded", function () {
         option.value = year;
         option.textContent = year;
         
-        select.appendChild(option);
+        yearSelect.appendChild(option);
     }
 
+    //
+    async function updateTrims() {
+        const model = modelSelect.value;
+        const year = yearSelect.value;
+
+        trimSelect.innerHTML = '<option selected disabled value="">Choose...</option>';
+        trimSelect.disabled = true;
+
+        //
+        if (model && year){
+            try{
+                const res = await fetch(`/admin/get_trims?model_name=${encodeURIComponent(model)}&year=${year}`);
+                const trims = await res.json();
+
+                if (trims.length > 0){
+                    trims.forEach(t => {
+                        const opt = document.createElement("option");
+                        opt.value = t;
+                        opt.textContent = t;
+                        trimSelect.appendChild(opt);
+                        
+                    });
+                    trimSelect.disabled = false;
+                }
+            } catch(err){
+                console.error("Error while loading trim: ", err);
+            }
+        }
+    }
+
+    modelSelect.addEventListener("change", updateTrims);
+    yearSelect.addEventListener("change", updateTrims);
 });
+
